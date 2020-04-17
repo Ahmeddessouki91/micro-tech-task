da9fd3abbf5e2415803427c00421054cd9329cc7
Ahmed Dessouki <eng.ahmed.dessouki91@gmail.com>
2020-04-17 17:44:46 +0200

    fix front

--- pctask-frontend/src/app/services/product.service.ts
+++ pctask-frontend/src/app/services/product.service.ts
@@ -9,30 +9,34 @@ import { Observable } from "rxjs";
   providedIn: "root"
 })
 export class ProductService {
-  constructor(private http: HttpClient) {}
+  private url = environment.baseURL;
+  constructor(private http: HttpClient) {
+    if (environment.production) {
+      this.url = process.env[environment.baseURL];
+    }
+  }
 
   createProduct(model: Product) {
-    return this.http.post(`${environment.baseURL}/product`, model);
+    return this.http.post(`${this.url}/product`, model);
   }
 
   updateProduct(model: Product) {
-    return this.http.put(`${environment.baseURL}/product`, model);
+    return this.http.put(`${this.url}/product`, model);
   }
 
   deleteProduct(id: number) {
-    return this.http.delete(`${environment.baseURL}/product/${id}`);
+    return this.http.delete(`${this.url}/product/${id}`);
   }
 
   getProduct(id: number) {
-    return this.http.get<Product>(`${environment.baseURL}/product/${id}`);
+    return this.http.get<Product>(`${this.url}/product/${id}`);
   }
 
   getAllProducts(filter: ProductFilter) {
     let filterParams = Object.keys(filter)
       .filter(key => filter[key] !== null)
       .map(key => `${key}=${filter[key]}`);
-    let url = `${environment.baseURL}/product?${filterParams.join("&")}`;
-    console.log(url);
+    let url = `${this.url}/product?${filterParams.join("&")}`;
     return this.http.get<Product[]>(url);
   }
 }
--- pctask-frontend/src/environments/environment.prod.ts
+++ pctask-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseURL: "https://localhost:5001/api"
+  baseURL: "DOMAIN"
 };
